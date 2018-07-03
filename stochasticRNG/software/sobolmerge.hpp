d7e90621871dd8f21918463406e119e814f3359f
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2018-07-03 15:24:50 -0500

    sobolmerge

---
+++ stochasticRNG/software/sobolmerge.hpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <iostream>
+using namespace std;
+#define min(X, Y)  ((X) < (Y) ? (X) : (Y))
+#define max(X, Y)  ((X) > (Y) ? (X) : (Y))
+
+class SOBOLMerge
+{
+    vector<vector<unsigned int>> inDirVec;
+    unsigned int bitLength;
+    unsigned int inDim;
+    unsigned int inLen;
+    vector<unsigned int> outMemVec;
+    string m_name;
+
+    public:
+        SOBOLMerge();
+        ~SOBOLMerge();
+        void Help();
+        void Init(vector<vector<unsigned int>>, unsigned int, string);
+        void Report();
+        void MemGen();
+        void VecPrint();
+        void MemPrint();
+        vector<unsigned int> OutMem();
+        unsigned int MemLen();
+};
---
+++ stochasticRNG/software/sobolmerge_test.hpp
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
