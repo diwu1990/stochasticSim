7574b2ddb8cd8222b3f9f938bc0e6494b0d02b7d
wudidaizi <wudidaizi@hotmail.com>
2018-07-19 14:02:38 -0500

    sqrt2

--- stochasticRNG/hardware/sqrt/sqrt.sv
+++ stochasticRNG/hardware/sqrt/sqrt.sv
@@ -18,7 +18,7 @@ module sqrt (
                 cnt <= cnt + 1;
             end else if(~inc & dec & ~|cnt) begin
                 cnt <= cnt - 1;
-            end else begin : 
+            end else begin
                 cnt <= cnt;
             end
         end
