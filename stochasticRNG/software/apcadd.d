0f766356e977ab9db301a8ffd786e2cce6d52fca
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2018-07-29 22:54:48 -0500

    2211213123

--- stochasticRNG/software/apcadd.d
+++
@@ -1,1 +0,0 @@
-apcadd.o: apcadd.cpp apcadd.hpp seqprobmulti.hpp crosscorrelation.hpp
--- stochasticRNG/software/apcadd_test.d
+++
@@ -1,4 +0,0 @@
-apcadd_test.o: apcadd_test.cpp main.hpp autocorrelation.hpp \
- crosscorrelation.hpp lfsr.hpp sobol.hpp xshrr1608.hpp randNum2Bit.hpp \
- randNum2BitMulti.hpp mul.hpp sobolmulti.hpp lfsrmulti.hpp seqsearch.hpp \
- sobolmerge.hpp apcadd.hpp
