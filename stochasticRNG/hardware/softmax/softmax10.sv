812b48db99ef64b1d52cfd885a2b743c518b420b
wudidaizi <wudidaizi@hotmail.com>
2018-09-02 21:40:00 -0500

    rm old

--- stochasticRNG/hardware/softmax/softmax10.sv
+++
@@ -1,8 +0,0 @@
-module softmax10 (
-	input clk,    // Clock
-	input clk_en, // Clock Enable
-	input rst_n,  // Asynchronous reset active low
-	
-);
-
-endmodule
--- stochasticRNG/hardware/sync/sync.sv
+++
@@ -1,8 +0,0 @@
-module sync (
-	input clk,    // Clock
-	input clk_en, // Clock Enable
-	input rst_n,  // Asynchronous reset active low
-	
-);
-
-endmodule
