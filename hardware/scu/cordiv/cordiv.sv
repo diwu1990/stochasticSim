497b727aa14617df7564657bb5d3d72cebd5c516
wudidaizi <wudidaizi@hotmail.com>
2018-09-05 18:08:37 -0500

    cordiv hw4

--- hardware/scu/cordiv/cordiv.sv
+++ hardware/scu/cordiv/cordiv.sv
@@ -15,7 +15,20 @@ module cordiv (
     logic srout;
 
     assign srout = SR[sel];
-
     assign quotient = divisor ? dividend : srout;
+
+    // <= SR[0] <= SR[1] <= quotient
+    always_ff @(posedge clk or negedge rst_n) begin : proc_SR
+        if(~rst_n) begin
+            SR <= 0;
+        end else begin
+            if(divisor == 1) begin
+                SR <= {SR[1], quotient};
+            end
+            else begin
+                SR <= SR;
+            end
+        end
+    end
 
 endmodule
