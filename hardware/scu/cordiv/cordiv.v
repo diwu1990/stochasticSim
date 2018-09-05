92c0a8c1f5c32eb1d1e49a65324c8ce43732e3eb
wudidaizi <wudidaizi@hotmail.com>
2018-09-05 17:43:00 -0500

    cordiv hw

---
+++ hardware/scu/cordiv/cordiv.v
@@ -0,0 +1,21 @@
+module CORDIV (
+	input clk,    // Clock
+	input rst_n,  // Asynchronous reset active low
+	input dividend,
+	input divisor,
+	input sel,
+	output quotient
+);
+    // define the depth of shift register
+    // 2 is recommended for accuracy according to simuation
+    parameter SRDEPTH = 2;
+
+    // shift register
+    logic [SRDEPTH-1 : 0] SR;
+    logic srout;
+
+    assign srout = SR[sel];
+
+    assign quotient = divisor ? dividend : srout;
+
+endmodule
