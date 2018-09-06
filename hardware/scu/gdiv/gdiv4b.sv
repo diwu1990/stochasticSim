ba5bdc6a93dbda55ad6e82f75a0f6c551e43888d
wudidaizi <wudidaizi@hotmail.com>
2018-09-06 01:00:32 -0500

    gdiv hw

--- hardware/scu/gdiv/gdiv4b.sv
+++ hardware/scu/gdiv/gdiv4b.sv
@@ -10,14 +10,19 @@ module gdiv4b (
     logic [3:0] cnt;
     logic inc;
     logic dec;
+    logic cntFull;
+    logic cntEmpty;
+
+    assign cntFull = &cnt;
+    assign cntEmpty = ~|cnt;
 
     always_ff @(posedge clk or negedge rst_n) begin : proc_cnt
         if(~rst_n) begin
             cnt <= 4'b1000;
         end else begin
-            if(inc & ~dec & ~&cnt) begin
+            if(inc & ~dec & ~cntFull) begin
                 cnt <= cnt + 1;
-            end else if(~inc & dec & ~|cnt) begin
+            end else if(~inc & dec & ~cntEmpty) begin
                 cnt <= cnt - 1;
             end else begin
                 cnt <= cnt;
--- hardware/scu/gdiv/gdiv5b.sv
+++ hardware/scu/gdiv/gdiv5b.sv
@@ -10,14 +10,19 @@ module gdiv5b (
     logic [4:0] cnt;
     logic inc;
     logic dec;
+    logic cntFull;
+    logic cntEmpty;
+
+    assign cntFull = &cnt;
+    assign cntEmpty = ~|cnt;
 
     always_ff @(posedge clk or negedge rst_n) begin : proc_cnt
         if(~rst_n) begin
             cnt <= 5'b10000;
         end else begin
-            if(inc & ~dec & ~&cnt) begin
+            if(inc & ~dec & ~cntFull) begin
                 cnt <= cnt + 1;
-            end else if(~inc & dec & ~|cnt) begin
+            end else if(~inc & dec & ~cntEmpty) begin
                 cnt <= cnt - 1;
             end else begin
                 cnt <= cnt;
