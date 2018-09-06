6f3a9229b6f5bff7ecd52eb6e1648df522dbfd11
wudidaizi <wudidaizi@hotmail.com>
2018-09-05 23:37:25 -0500

    skewedSync hw2

--- hardware/correlation/skewedSync/skewedSync.sv
+++ hardware/correlation/skewedSync/skewedSync.sv
@@ -8,6 +8,8 @@ module skewedSync (
 
     // bit array of index 1 has a higher value than that of index 0
 
+    logic [1:0] out;
+
     logic [DEPTH - 1 : 0] cnt;
     logic cntFull;
     logic cntEmpty;
@@ -17,11 +19,6 @@ module skewedSync (
     assign out[1] = in[1];
 
     always_comb begin : proc_out0
-        if((in[1] & ~in[0] & ~|cnt) | (in[1] & in[0])) begin
-            out[0] = 1;
-        end else begin
-            out[0] <= 0;
-        end
         if(in[0] == in[1]) begin
             out[0] = in[0];
         end else begin
