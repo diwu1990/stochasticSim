adb8d373f9073fbaab7955885ee095cff35f53e4
wudidaizi <wudidaizi@hotmail.com>
2018-09-07 14:51:17 -0500

    gsqrt sw2

---
+++ software/scu/gsqrt/gsqrt.hpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <iostream>
+using namespace std;
+
+#define min(X, Y)  ((X) < (Y) ? (X) : (Y))
+#define max(X, Y)  ((X) > (Y) ? (X) : (Y))
+
+class GSQRT
+{
+    vector<unsigned int> inSeq;
+    float inProb;
+    float inAC;
+    float outAC;
+    vector<unsigned int> randNum;
+    unsigned int bitLength;
+    unsigned int logDepth;
+
+    unsigned int seqLength;
+    vector<unsigned int> outSeq;
+    float theoProb;
+    vector<float> realProb;
+    vector<float> errRate;
+    string m_name;
+    unsigned int lowErrLen;
+    unsigned int ppStage;
+
+    public:
+        GSQRT();
+        ~GSQRT();
+        void Help();
+        void Init(vector<unsigned int>, vector<unsigned int>, unsigned int, string);
+        void Report();
+        void Calc();
+        void OutPrint();
+        vector<unsigned int> OutSeq();
+        float InProb();
+        float InAC();
+        float OutAC();
+        float TheoProb();
+        vector<float> RealProb();
+        float FinalRealProb();
+        vector<float> ErrRate();
+        float FinalErrRate();
+        unsigned int SeqLen();
+        unsigned int LowErrLen();
+        unsigned int PPStage();
+};
