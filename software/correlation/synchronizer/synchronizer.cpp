9fda7526502288c8e285f6b96aa107d8c2e1f608
wudidaizi <wudidaizi@hotmail.com>
2018-09-29 17:40:57 -0500

    sync sw

--- software/correlation/synchronizer/synchronizer.cpp
+++ software/correlation/synchronizer/synchronizer.cpp
@@ -10,7 +10,6 @@ void Synchronizer::Help()
     printf("1. inst.Init() method:\n");
     printf("Configure the current inst.\n");
     printf("Parameters: Input Probability, Buffer Depth, Window Size, Threshold for Window Bias, Instance Name.\n");
-    printf("Buffer Depth is not used in this class, always default to 1.\n");
 
     printf("2. inst.Calc() method:\n");
     printf("Match logic ones with logic zeros for max negative correlation with configuration in inst.Init().\n");
