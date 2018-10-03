d34fc347db3e20d8a64af9986b1818648e138705
wudidaizi <wudidaizi@hotmail.com>
2018-10-03 14:29:11 -0500

    cordiv sw5

--- software/scu/cordiv/cordiv.cpp
+++ software/scu/cordiv/cordiv.cpp
@@ -16,6 +16,9 @@ void CORDIV::Help()
     printf("2. inst.Calc() method:\n");
     printf("Calculate the result bit.\n");
     printf("Parameters: Vectorized Input Bits, Vectorized Random Number.\n");
+    printf("Input bit 0 is dividend, and input bit 1 is divisor.\n");
+    printf("The 0th value of vectorized Random Number is for the Synchronizer.\n");
+    printf("The 1th value of vectorized Random Number is for the Trace Reg.\n");
 
     printf("3. inst.OutBit() method:\n");
     printf("Return output bit from inst.Calc().\n");
