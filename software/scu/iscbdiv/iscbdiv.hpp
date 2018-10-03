7680dea4e592230ac15559741616ecad5b2d10b1
wudidaizi <wudidaizi@hotmail.com>
2018-10-03 16:07:36 -0500

    iscbdiv sw

--- software/scu/iscbdiv/iscbdiv.hpp
+++ software/scu/iscbdiv/iscbdiv.hpp
@@ -1,8 +1,11 @@
-#include <stdio.h>
+#include <cstdio>
 #include <vector>
 #include <cmath>
 #include <string>
 #include <iostream>
+#include "perfsim.hpp"
+#include "skewedsynchronizer.hpp"
+#include "cordivkernel.hpp"
 using namespace std;
 
 #define min(X, Y)  ((X) < (Y) ? (X) : (Y))
@@ -10,40 +13,50 @@ using namespace std;
 
 class ISCBDIV
 {
-    vector<vector<char>> inSeq;
-    vector<float> inProb;
-    vector<unsigned int> randNum;
-    unsigned int depth;
+    // initial input
+    vector<float> iProb;
     unsigned int depthSync;
-    unsigned int inDim;
-
-    unsigned int seqLength;
-    vector<char> outSeq;
-    float inCC;
-    float theoProb;
-    vector<float> realProb;
-    vector<float> errRate;
+    unsigned int depth;
+    unsigned int wSize;
+    float thdBias;
     string m_name;
-    unsigned int lowErrLen;
-    unsigned int ppStage;
 
-    public:
-        ISCBDIV();
-        ~ISCBDIV();
-        void Help();
-        void Init(vector<vector<char>>, vector<unsigned int>, unsigned int, unsigned int, string);
-        void Report();
-        void Calc();
-        void OutPrint();
-        vector<char> OutSeq();
-        float InCC();
-        vector<float> InProb();
-        float TheoProb();
-        vector<float> RealProb();
-        float FinalRealProb();
-        vector<float> ErrRate();
-        float FinalErrRate();
-        unsigned int SeqLen();
-        unsigned int LowErrLen();
-        unsigned int PPStage();
+    // calc input
+    vector<char> iBit;
+    vector<unsigned int> randNum;
+
+    // internal
+    unsigned int iDim;
+    unsigned int oDim;
+    #ifdef PERFSIM
+        unsigned int iLen;
+    #endif
+    SkewedSynchronizer skewedSyncInst;
+    CORDIVKERNEL cordivKernelInst;
+
+    // output
+    vector<char> oBit;
+
+    // perfsim output
+    #ifdef PERFSIM
+        vector<vector<char>> oBS;
+        vector<float> wProb;
+        vector<float> theoProb;
+        vector<float> wBias;
+        vector<unsigned int> speed;
+    #endif
+
+public:
+    void Help();
+    void Init(vector<float>, unsigned int, unsigned int, unsigned int, float, string);
+    void Calc(vector<char>, vector<unsigned int>);
+    vector<char> OutBit();
+
+    #ifdef PERFSIM
+        vector<vector<char>> OutBS();
+        vector<float> WProb();
+        vector<float> TheoProb();
+        vector<float> WBias();
+        vector<unsigned int> Speed();
+    #endif
 };
