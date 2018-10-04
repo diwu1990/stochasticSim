4f2d57d6eb2479f69dac55627d0911e5d270fa94
wudidaizi <wudidaizi@hotmail.com>
2018-10-03 21:21:29 -0500

    jkdivbisqrt sw

--- software/scu/jkdivbisqrt/jkdivbisqrt.cpp
+++ software/scu/jkdivbisqrt/jkdivbisqrt.cpp
@@ -1,280 +1,172 @@
 #include "jkdivbisqrt.hpp"
-#include "seqprob.hpp"
-#include "autocorrelation.hpp"
-#include "crosscorrelation.hpp"
+#include "perfsim.hpp"
 
-JKDIVBISQRT::JKDIVBISQRT(){}
-JKDIVBISQRT::~JKDIVBISQRT(){}
 void JKDIVBISQRT::Help()
 {
     printf("**********************************************************\n");
     printf("**********************************************************\n");
     printf("Calling JKDIVBISQRT Help. Following are instructions to JKDIVBISQRT Instance Usage:\n");
+
     printf("1. inst.Init() method:\n");
-    printf("Configure the JKDIVBISQRT inst.\n");
-    printf("Initial Parameters: Input Vector, Random Number Seqsence, Trace Register Depth, Instance Name.\n");
-    printf("Trace Register Depth is not used for JKDIVBISQRT.\n");
+    printf("Configure the current inst.\n");
+    printf("Parameters: Input Probability, Depth of Synchronizer, Depth of Cordiv Kernel, Window Size, Threshold for Window Bias, Instance Name.\n");
+    printf("Input 0 is the valid input.\n");
+    printf("Both Depth of Synchronizer and Depth of Cordiv Kernel are not used.");
 
     printf("2. inst.Calc() method:\n");
-    printf("Calculate the quotient of two input sequences.\n");
+    printf("Calculate the result bit.\n");
+    printf("Parameters: Vectorized Input Bits, Vectorized Random Number.\n");
+    printf("Input 0 is the valid input.\n");
+    printf("Both the 0th and the 1th value of vectorized Random Number are not used.\n");
 
-    printf("3. inst.OutSeq() method:\n");
-    printf("Return the calculated result.\n");
+    printf("3. inst.OutBit() method:\n");
+    printf("Return output bit from inst.Calc().\n");
 
-    printf("4. inst.OutPrint() method:\n");
-    printf("Print the information of result.\n");
+    printf(">>>>>>>>The following methods require macro definition of PERFSIM.<<<<<<<<\n");
+    printf("4. inst.OutBS() method:\n");
+    printf("Return output bit stream from inst.Calc().\n");
 
-    printf("5. inst.InAC() method:\n");
-    printf("Return the input auto-correlation.\n");
+    printf("5. inst.RealProb() method:\n");
+    printf("Return the real probability.\n");
 
-    printf("6. inst.InProb() method:\n");
-    printf("Return the input probability.\n");
+    printf("6. inst.TheoProb() method:\n");
+    printf("Return the theoretical probability.\n");
 
-    printf("7. inst.TheoProb() method:\n");
-    printf("Return the theoretical output probability.\n");
+    printf("7. inst.WBias() method:\n");
+    printf("Return the window bias.\n");
 
-    printf("8. inst.RealProb() method:\n");
-    printf("Return the array of progressive output probability.\n");
-
-    printf("9. inst.FinalRealProb() method:\n");
-    printf("Return the final output probability.\n");
-
-    printf("10. inst.ErrRate() method:\n");
-    printf("Return the array of progressive output error rate.\n");
-
-    printf("11. inst.FinalErrRate() method:\n");
-    printf("Return the final output error rate.\n");
-
-    printf("12. inst.SeqLen() method:\n");
-    printf("Return the sequence length.\n");
-
-    printf("13. inst.LowErrLen() method:\n");
-    printf("Return the sequence length required to converge with less than 5 percent error rate.\n");
-
-    printf("14. inst.PPStage() method:\n");
-    printf("Return the pipline stages required by hardware.\n");
-
-    printf("15. inst.Report() method:\n");
-    printf("Report the current instance.\n");
+    printf("8. inst.Speed() method:\n");
+    printf("Return the converge speed.\n");
     printf("**********************************************************\n");
     printf("**********************************************************\n");
 }
 
-void JKDIVBISQRT::Init(vector<char> param1, vector<unsigned int> param2, unsigned int param3, string param4)
+void JKDIVBISQRT::Init(vector<float> param1, unsigned int param2, unsigned int param3, unsigned int param4, float param5, string param6)
 {
-    inSeq = param1;
-    SeqProb probCalc;
-    probCalc.Init(inSeq,"probCalc");
-    probCalc.Calc();
-    inProb = probCalc.OutProb();
-    randNum = param2;
+    iProb = param1;
+    depthSync = param2;
     depth = param3;
-    m_name = param4;
+    wSize = param4;
+    thdBias = param5;
+    m_name = param6;
 
-    seqLength = (unsigned int)inSeq.size();
-    theoProb = sqrt(inProb);
-    outSeq.resize(seqLength);
-    realProb.resize(seqLength);
-    errRate.resize(seqLength);
-    for (int i = 0; i < seqLength; ++i)
+    iDim = (unsigned int)iProb.size();
+    if (iDim != 2)
     {
-        outSeq[i] = 0;
-        realProb[i] = 0;
-        errRate[i] = 0;
+        printf("Error: Input dimension is not 2.\n");
     }
-    lowErrLen = 0;
-    ppStage = 0;
+    oDim = 1;
+    #ifdef PERFSIM
+        iLen = 0;
+    #endif
+
+    muxProb.resize(2);
+    muxInBit.resize(2);
+    muxProb[0] = 1;
+    muxProb[1] = iProb[0];
+    muxInst.Init(muxProb, wSize, thdBias, "muxInst");
+    traceProb.resize(2);
+    traceInBit.resize(2);
+    traceProb[0] = 1;
+    traceProb[1] = sqrt(iProb[0]);
+    traceInst.Init(traceProb, wSize, thdBias, "traceInst");
+
+    oBit.resize(oDim);
+    for (int i = 0; i < oDim; ++i)
+    {
+        oBit[i] = 0;
+    }
+    
+    #ifdef PERFSIM
+        oBS.resize(oDim);
+        wProb.resize(oDim);
+        theoProb.resize(oDim);
+        wBias.resize(oDim);
+        speed.resize(oDim);
+
+        for (int i = 0; i < oDim; ++i)
+        {
+            wProb[i] = 0;
+            theoProb[i] = sqrt(iProb[0]);
+            speed[i] = 0;
+        }
+    #endif
 }
 
-void JKDIVBISQRT::Report()
+void JKDIVBISQRT::Calc(vector<char> param1, vector<unsigned int> param2)
 {
-    printf("Current JKDIVBISQRT:\n");
-    std::cout << "Instance name:          " << m_name << std::endl;
-    printf("Seqsence Length:        %u\n", seqLength);
-    printf("Input Probability:      %f\n", inProb);
-    printf("Theoretical Probability:%f\n", theoProb);
-}
-
-// void JKDIVBISQRT::CalcQuot()
-void JKDIVBISQRT::Calc()
-{
-    AutoCorrelation autocorrelationInst;
-    autocorrelationInst.Init(inSeq, 1, inProb, "autocorrelationInst");
-    autocorrelationInst.Calc();
-    inAC = autocorrelationInst.OutAC();
-    unsigned int accuracyLength = seqLength/2;
-
+    iBit = param1;
+    randNum = param2;
 
     // *****************************************************************************
-    // bit inserting
+    // counter based no correlation
     // *****************************************************************************
-    vector<char> JKFF(seqLength);
-    for (int i = 0; i < seqLength; ++i)
+
+    muxInBit[0] = 1;
+    muxInBit[1] = iBit[0];
+    muxInNum[0] = (unsigned int)traceInst.OutBit()[0];
+    muxInst.Calc(muxInBit, muxInNum);
+    traceInBit[0] = 1;
+    traceInBit[1] = muxInst.OutBit()[0];
+    traceInst.Calc(traceInBit);
+
+    oBit[0] = muxInst.OutBit()[0];
+
+    #ifdef PERFSIM
+        iLen++;
+        vector<unsigned int> totalSum(oDim);
+        for (int i = 0; i < oDim; ++i)
+        {
+            totalSum[i] = 0;
+            oBS[i].push_back(oBit[i]);
+            if (oBS[i].size() < wSize)
+            {
+                for (int j = 0; j < oBS[i].size(); ++j)
+                {
+                    totalSum[i] += oBS[i][j];
+                }
+                wProb[i] = (float)totalSum[i]/iLen;
+            }
+            else
+            {
+                for (int j = oBS[i].size() - wSize; j < oBS[i].size(); ++j)
+                {
+                    totalSum[i] += oBS[i][j];
+                }
+                wProb[i] = (float)totalSum[i]/wSize;
+            }
+            wBias[i] = wProb[i] - theoProb[i];
+            if ((wBias[i] > thdBias) || (wBias[i] < (0-thdBias)))
+            {
+                speed[i] = iLen;
+            }
+        }
+    #endif
+}
+
+vector<char> JKDIVBISQRT::OutBit()
+{
+    return oBit;
+}
+
+#ifdef PERFSIM
+    vector<float> JKDIVBISQRT::WProb()
     {
-        JKFF[i] = 1;
-    }
-    unsigned int oneCount = 0;
-    unsigned int sel = 1;
-
-    vector<char> selSeq(seqLength); // add for muxSCC and traceSAC
-
-    for (int i = 0; i < seqLength; ++i)
-    {
-        // printf("%u,", sel);
-        selSeq[i] = sel; // add for muxSCC and traceSAC
-        if (sel == 1)
-        {
-            outSeq[i] = inSeq[i];
-        }
-        else
-        {
-            outSeq[i] = 1;
-        }
-        oneCount += outSeq[i];
-        if (i < accuracyLength)
-        {
-            realProb[i] = (float)oneCount/(float)(i+1);
-        }
-        else
-        {
-            realProb[i] = (realProb[i-1]*(float)accuracyLength+outSeq[i]-outSeq[i-accuracyLength])/(float)accuracyLength;
-        }
-        // errRate[i] = (theoProb - realProb[i])/theoProb;
-        errRate[i] = (theoProb - realProb[i]);
-
-        // applying a JK FF
-        // J is always 1.
-        // K is outSeq[i].
-        // if (outSeq[(i+seqLength-1)%seqLength] == 1)
-        if (outSeq[i] == 1)
-        {
-            sel = 1-sel;
-            JKFF[i] = sel;
-        }
-        else
-        {
-            sel = 1;
-            JKFF[i] = sel;
-        }
-    }
-
-    for (int i = 0; i < seqLength; ++i)
-    {
-        if (errRate[seqLength-1-i] > 0.05 || errRate[seqLength-1-i] < -0.05)
-        {
-            lowErrLen = seqLength-i;
-            break;
-        }
+        return wProb;
     }
 
-    // muxSCC
-    vector<vector<char>> muxSCCSeq(2);
-    muxSCCSeq[0].resize(seqLength);
-    muxSCCSeq[0] = inSeq;
-    muxSCCSeq[1].resize(seqLength);
-    muxSCCSeq[1] = selSeq;
-    
-    CrossCorrelation muxSCCInst;
-    muxSCCInst.Init(muxSCCSeq, 1, "muxSCCInst");
-    muxSCCInst.Calc();
-    muxSCC = muxSCCInst.OutCC()[0];
+    vector<float> JKDIVBISQRT::TheoProb()
+    {
+        return theoProb;
+    }
 
-    // traceSAC
-    float traceSACProb;
+    vector<float> JKDIVBISQRT::WBias()
+    {
+        return wBias;
+    }
 
-    SeqProb selProbCalc;
-    selProbCalc.Init(outSeq,"selProbCalc");
-    selProbCalc.Calc();
-    traceSACProb = selProbCalc.OutProb();
-    
-    AutoCorrelation traceSACInst;
-    traceSACInst.Init(outSeq, 1, realProb[seqLength-1], "traceSACInst");
-    traceSACInst.Calc();
-    traceSAC = traceSACInst.OutAC();
-    // printf("%f,%f\n", traceSACProb,traceSAC);
-}
-
-float JKDIVBISQRT::TraceSAC()
-{
-    return traceSAC;
-}
-
-float JKDIVBISQRT::MuxSCC()
-{
-    return muxSCC;
-}
-
-vector<char> JKDIVBISQRT::OutSeq()
-{
-    return outSeq;
-}
-
-unsigned int JKDIVBISQRT::PPStage()
-{
-    return ppStage;
-}
-
-void JKDIVBISQRT::OutPrint()
-{
-    printf("Calling OutPrint for JKDIVBISQRT instance: ");
-    std::cout << m_name << std::endl;
-    printf("Theoretical Probability: sqrt(%.3f) = %.3f with input autocorrelation %.3f\n", inProb, theoProb, inAC);
-    printf("Final Probability: %.3f with Error Rate: %.3f\n", realProb[seqLength-1], errRate[seqLength-1]);
-    printf("Low Error Length (5 percent approximation): %u\n", lowErrLen);
-    // for (int i = 0; i < seqLength; ++i)
-    // {
-    //     printf("%.3f,", errRate[i]);
-    // }
-    // printf("\n");
-    // printf("Output Probability:\n");
-    // for (int i = 0; i < seqLength; ++i)
-    // {
-    //     printf("%.3f,", realProb[i]);
-    // }
-    // printf("\n");
-}
-
-float JKDIVBISQRT::InAC()
-{
-    return inAC;
-}
-
-float JKDIVBISQRT::InProb()
-{
-    return inProb;
-}
-
-float JKDIVBISQRT::TheoProb()
-{
-    return theoProb;
-}
-
-vector<float> JKDIVBISQRT::RealProb()
-{
-    return realProb;
-}
-
-float JKDIVBISQRT::FinalRealProb()
-{
-    return realProb[seqLength-1];
-}
-
-vector<float> JKDIVBISQRT::ErrRate()
-{
-    return errRate;
-}
-
-float JKDIVBISQRT::FinalErrRate()
-{
-    return errRate[seqLength-1];
-}
-
-unsigned int JKDIVBISQRT::SeqLen()
-{
-    return seqLength;
-}
-
-unsigned int JKDIVBISQRT::LowErrLen()
-{
-    return lowErrLen;
-}
+    vector<unsigned int> JKDIVBISQRT::Speed()
+    {
+        return speed;
+    }
+#endif
