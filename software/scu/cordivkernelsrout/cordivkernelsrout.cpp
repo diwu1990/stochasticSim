fb71bdb5295434c5a488f652f235ec718ae396cf
wudidaizi <wudidaizi@hotmail.com>
2018-10-06 16:52:32 -0500

    cordivkernelsrout char sw

--- software/scu/cordivkernelsrout/cordivkernelsrout.cpp
+++ software/scu/cordivkernelsrout/cordivkernelsrout.cpp
@@ -19,7 +19,7 @@ void CORDIVKERNELSROUT::Help()
     printf("Parameters: Vectorized Input Bits, Vectorized Random Number.\n");
     printf("Input bit 0 is dividend, and input bit 1 is divisor.\n");
     printf("The 0th value of vectorized Random Number is for the Synchronizer and not used.\n");
-    printf("The 1th value of vectorized Random Number is for the Rrace Reg.\n");
+    printf("The 1th value of vectorized Random Number is for the CORDIV Kernel.\n");
 
     printf("3. inst.OutBit() method:\n");
     printf("Return output bit from inst.Calc().\n");
