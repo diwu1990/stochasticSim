f32fb53b2cf900818aebc1f8a90c9d7f0c948f96
wudidaizi <wudidaizi@hotmail.com>
2018-10-07 17:26:32 -0500

    cyclesim jkdivbisqrt_h

--- software/scu/jkdivbisqrt/jkdivbisqrt.hpp
+++ software/scu/jkdivbisqrt/jkdivbisqrt.hpp
@@ -1,3 +1,7 @@
+#pragma once
+#ifndef JKDIVBISQRT_H
+#define JKDIVBISQRT_H
+
 #include <cstdio>
 #include <vector>
 #include <cmath>
@@ -65,3 +69,5 @@ public:
         vector<unsigned int> Speed();
     #endif
 };
+
+#endif
