0f33da6e004ceb15adfbf21311803571a20fc427
wudidaizi <wudidaizi@hotmail.com>
2018-10-07 17:17:16 -0500

    cyclesim cordiv_h

--- software/scu/cordiv/cordiv.hpp
+++ software/scu/cordiv/cordiv.hpp
@@ -1,3 +1,7 @@
+#pragma once
+#ifndef CORDIV_H
+#define CORDIV_H
+
 #include <cstdio>
 #include <vector>
 #include <cmath>
@@ -62,3 +66,5 @@ public:
         vector<unsigned int> Speed();
     #endif
 };
+
+#endif
