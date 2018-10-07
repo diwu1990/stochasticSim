cf050a65769390cad86c229bc40f3e8f0383f8a7
wudidaizi <wudidaizi@hotmail.com>
2018-10-07 17:29:17 -0500

    cyclesim relu_h

--- software/scu/relu/relu.hpp
+++ software/scu/relu/relu.hpp
@@ -1,3 +1,7 @@
+#pragma once
+#ifndef RELU_H
+#define RELU_H
+
 #include <cstdio>
 #include <vector>
 #include <cmath>
@@ -58,3 +62,5 @@ public:
         vector<unsigned int> Speed();
     #endif
 };
+
+#endif
