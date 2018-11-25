28fab54f0ecbeb21a2546736e8247f1f4d4dcae5
wudidaizi <wudidaizi@hotmail.com>
2018-11-25 16:21:53 -0600

    cyclesim jkdivbisqrt

--- software/scu/jkdivbisqrt/jkdivbisqrt.cpp
+++ software/scu/jkdivbisqrt/jkdivbisqrt.cpp
@@ -35,8 +35,8 @@ void JKDIVBISQRT::Help()
     printf("7. inst.WBias() method:\n");
     printf("Return the window bias.\n");
 
-    printf("8. inst.Speed() method:\n");
-    printf("Return the converge speed.\n");
+    printf("8. inst.CTime() method:\n");
+    printf("Return the converge ctime.\n");
     printf("**********************************************************\n");
     printf("**********************************************************\n");
 }
@@ -83,13 +83,13 @@ void JKDIVBISQRT::Init(vector<float> param1, unsigned int param2, unsigned int p
         wProb.resize(oDim);
         theoProb.resize(oDim);
         wBias.resize(oDim);
-        speed.resize(oDim);
+        ctime.resize(oDim);
 
         for (int i = 0; i < oDim; ++i)
         {
             wProb[i] = 0;
             theoProb[i] = sqrt(iProb[0]);
-            speed[i] = 0;
+            ctime[i] = 0;
         }
     #endif
 }
@@ -139,7 +139,7 @@ void JKDIVBISQRT::Calc(vector<char> param1, vector<unsigned int> param2)
             wBias[i] = wProb[i] - theoProb[i];
             if ((wBias[i] > thdBias) || (wBias[i] < (0-thdBias)))
             {
-                speed[i] = iLen;
+                ctime[i] = iLen;
             }
         }
     #endif
@@ -166,8 +166,8 @@ vector<char> JKDIVBISQRT::OutBit()
         return wBias;
     }
 
-    vector<unsigned int> JKDIVBISQRT::Speed()
+    vector<unsigned int> JKDIVBISQRT::CTime()
     {
-        return speed;
+        return ctime;
     }
 #endif
--- software/scu/jkdivbisqrt/jkdivbisqrt.hpp
+++ software/scu/jkdivbisqrt/jkdivbisqrt.hpp
@@ -52,7 +52,7 @@ class JKDIVBISQRT
         vector<float> wProb;
         vector<float> theoProb;
         vector<float> wBias;
-        vector<unsigned int> speed;
+        vector<unsigned int> ctime;
     #endif
 
 public:
@@ -66,7 +66,7 @@ public:
         vector<float> WProb();
         vector<float> TheoProb();
         vector<float> WBias();
-        vector<unsigned int> Speed();
+        vector<unsigned int> CTime();
     #endif
 };
 
