c95946e33c02f019fe885e4cac2ded115089b58c
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2019-08-11 17:43:47 -0700

    uMUL

---
+++ hardware/scu/uMUL/uMUL.sv
@@ -0,0 +1,38 @@
+`include "SobolRNGDim1.sv"
+`define DATAWD `INWD 
+
+module uMUL (
+    input logic clk,    // Clock
+    input logic rst_n,  // Asynchronous reset active low
+    input logic iA,
+    input logic [`DATAWD-1:0] iB,
+    input logic loadB,
+    input logic oC,
+);
+    
+    logic [`DATAWD-1:0] iB_buf;
+
+    always_ff @(posedge clk or negedge rst_n) begin : proc_iB_buf
+        if(~rst_n) begin
+            iB_buf <= 0;
+        end else begin
+            if(loadB) begin
+                iB_buf <= iB;
+            end else begin
+                iB_buf <= iB_buf;
+            end
+        end
+    end
+
+    SobolRNGDim1 U_SobolRNGDim1(
+        .clk(clk),
+        .rst_n(rst_n),
+        .enable(iA),
+        .sobolSeq(sobolSeq)
+        );
+
+    always_comb begin : proc_oC
+        oC <= iA & (iB_buf > sobolSeq);
+    end
+
+endmodule
