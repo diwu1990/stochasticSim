2a46cc358a99655887009524465c437c953ec4ef
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2019-08-12 02:48:55 -0700

    fix bug in bADD.sv

--- hardware/scu/bADD/bADD.sv
+++ hardware/scu/bADD/bADD.sv
@@ -13,8 +13,12 @@ module bADD (
     always_ff @(posedge clk or negedge rst_n) begin : proc_oC
         if(~rst_n) begin
             oC <= 0;
+            iA_buf <= 0;
+            iB_buf <= 0;
         end else begin
-            oC <= iA + iB;
+            iA_buf <= iA;
+            iB_buf <= iB;
+            oC <= iA_buf + iB_buf;
         end
     end
 
