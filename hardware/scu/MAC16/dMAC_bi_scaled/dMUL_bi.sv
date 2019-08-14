68693a9ffa579db816c70d0f42ddcb8e725c942f
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2019-08-14 02:49:08 -0700

    dMAC_bi_scaled

---
+++ hardware/scu/MAC16/dMAC_bi_scaled/dMUL_bi.sv
@@ -0,0 +1,47 @@
+`include "SobolRNGDim1.sv"
+`define DATAWD `INWD 
+
+module dMUL_bi (
+    input logic clk,    // Clock
+    input logic rst_n,  // Asynchronous reset active low
+    input logic [`DATAWD-1:0] iA,
+    input logic [`DATAWD-1:0] iB,
+    input logic loadA,
+    input logic loadB,
+    input logic [`DATAWD-1:0] sobolSeqA,
+    input logic [`DATAWD-1:0] sobolSeqB,
+    output logic oC
+);
+    
+    logic [`DATAWD-1:0] iA_buf;
+    logic [`DATAWD-1:0] iB_buf;
+
+    always_ff @(posedge clk or negedge rst_n) begin : proc_iA_buf
+        if(~rst_n) begin
+            iA_buf <= 0;
+        end else begin
+            if(loadA) begin
+                iA_buf <= iA;
+            end else begin
+                iA_buf <= iA_buf;
+            end
+        end
+    end
+
+    always_ff @(posedge clk or negedge rst_n) begin : proc_iB_buf
+        if(~rst_n) begin
+            iB_buf <= 0;
+        end else begin
+            if(loadB) begin
+                iB_buf <= iB;
+            end else begin
+                iB_buf <= iB_buf;
+            end
+        end
+    end
+
+    always_comb begin : proc_oC
+        oC <= ~((iA_buf > sobolSeqA) ^ (iB_buf > sobolSeqB));
+    end
+
+endmodule
