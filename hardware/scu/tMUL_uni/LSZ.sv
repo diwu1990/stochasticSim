cda55ae3b45879d8bad360f3682ebbcf5a91134b
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2019-08-14 01:22:28 -0700

    tMUL_uni 3

--- hardware/scu/tMUL_uni/LSZ.sv
+++ hardware/scu/tMUL_uni/LSZ.sv
@@ -24,6 +24,42 @@ module LSZ (
         end
     endgenerate
 
+    `ifdef INWD3
+        always_comb begin : proc_3
+            case(outoh)
+                'b001 : lszIdx = 'd0;
+                'b010 : lszIdx = 'd1;
+                'b100 : lszIdx = 'd2;
+                default : lszIdx = 'd0;
+            endcase // onehot
+        end
+    `endif
+
+    `ifdef INWD4
+        always_comb begin : proc_4
+            case(outoh)
+                'b0001 : lszIdx = 'd0;
+                'b0010 : lszIdx = 'd1;
+                'b0100 : lszIdx = 'd2;
+                'b1000 : lszIdx = 'd3;
+                default : lszIdx = 'd0;
+            endcase // onehot
+        end
+    `endif
+
+    `ifdef INWD5
+        always_comb begin : proc_5
+            case(outoh)
+                'b00001 : lszIdx = 'd0;
+                'b00010 : lszIdx = 'd1;
+                'b00100 : lszIdx = 'd2;
+                'b01000 : lszIdx = 'd3;
+                'b10000 : lszIdx = 'd4;
+                default : lszIdx = 'd0;
+            endcase // onehot
+        end
+    `endif
+
     `ifdef INWD6
         always_comb begin : proc_6
             case(outoh)
@@ -36,40 +72,72 @@ module LSZ (
                 default : lszIdx = 'd0;
             endcase // onehot
         end
-    `else
-        `ifdef INWD8
-            always_comb begin : proc_6
-                case(outoh)
-                    'b00000001 : lszIdx = 'd0;
-                    'b00000010 : lszIdx = 'd1;
-                    'b00000100 : lszIdx = 'd2;
-                    'b00001000 : lszIdx = 'd3;
-                    'b00010000 : lszIdx = 'd4;
-                    'b00100000 : lszIdx = 'd5;
-                    'b01000000 : lszIdx = 'd6;
-                    'b10000000 : lszIdx = 'd7;
-                    default : lszIdx = 'd0;
-                endcase // onehot
-            end
-        `else 
-            `ifdef INWD10
-                always_comb begin : proc_10
-                    case(outoh)
-                        'b0000000001 : lszIdx = 'd0;
-                        'b0000000010 : lszIdx = 'd1;
-                        'b0000000100 : lszIdx = 'd2;
-                        'b0000001000 : lszIdx = 'd3;
-                        'b0000010000 : lszIdx = 'd4;
-                        'b0000100000 : lszIdx = 'd5;
-                        'b0001000000 : lszIdx = 'd6;
-                        'b0010000000 : lszIdx = 'd7;
-                        'b0100000000 : lszIdx = 'd8;
-                        'b1000000000 : lszIdx = 'd9;
-                        default : lszIdx = 'd0;
-                    endcase // onehot
-                end
-            `endif
-        `endif
+    `endif
+
+    `ifdef INWD7
+        always_comb begin : proc_7
+            case(outoh)
+                'b0000001 : lszIdx = 'd0;
+                'b0000010 : lszIdx = 'd1;
+                'b0000100 : lszIdx = 'd2;
+                'b0001000 : lszIdx = 'd3;
+                'b0010000 : lszIdx = 'd4;
+                'b0100000 : lszIdx = 'd5;
+                'b1000000 : lszIdx = 'd6;
+                default : lszIdx = 'd0;
+            endcase // onehot
+        end
+    `endif
+
+    `ifdef INWD8
+        always_comb begin : proc_8
+            case(outoh)
+                'b00000001 : lszIdx = 'd0;
+                'b00000010 : lszIdx = 'd1;
+                'b00000100 : lszIdx = 'd2;
+                'b00001000 : lszIdx = 'd3;
+                'b00010000 : lszIdx = 'd4;
+                'b00100000 : lszIdx = 'd5;
+                'b01000000 : lszIdx = 'd6;
+                'b10000000 : lszIdx = 'd7;
+                default : lszIdx = 'd0;
+            endcase // onehot
+        end
+    `endif
+
+    `ifdef INWD9
+        always_comb begin : proc_9
+            case(outoh)
+                'b000000001 : lszIdx = 'd0;
+                'b000000010 : lszIdx = 'd1;
+                'b000000100 : lszIdx = 'd2;
+                'b000001000 : lszIdx = 'd3;
+                'b000010000 : lszIdx = 'd4;
+                'b000100000 : lszIdx = 'd5;
+                'b001000000 : lszIdx = 'd6;
+                'b010000000 : lszIdx = 'd7;
+                'b100000000 : lszIdx = 'd8;
+                default : lszIdx = 'd0;
+            endcase // onehot
+        end
+    `endif
+
+    `ifdef INWD10
+        always_comb begin : proc_10
+            case(outoh)
+                'b0000000001 : lszIdx = 'd0;
+                'b0000000010 : lszIdx = 'd1;
+                'b0000000100 : lszIdx = 'd2;
+                'b0000001000 : lszIdx = 'd3;
+                'b0000010000 : lszIdx = 'd4;
+                'b0000100000 : lszIdx = 'd5;
+                'b0001000000 : lszIdx = 'd6;
+                'b0010000000 : lszIdx = 'd7;
+                'b0100000000 : lszIdx = 'd8;
+                'b1000000000 : lszIdx = 'd9;
+                default : lszIdx = 'd0;
+            endcase // onehot
+        end
     `endif
 
 endmodule
--- hardware/scu/tMUL_uni/SobolRNGDim1.sv
+++ hardware/scu/tMUL_uni/SobolRNGDim1.sv
@@ -28,6 +28,33 @@ module SobolRNGDim1 (
         );
 
     /* initialization of directional vectors for current dimension*/
+    `ifdef INWD3
+        always_comb begin : proc_dirVec_3
+            dirVec[0] <= 'd4;
+            dirVec[1] <= 'd2;
+            dirVec[2] <= 'd1;
+        end
+    `endif
+
+    `ifdef INWD4
+        always_comb begin : proc_dirVec_4
+            dirVec[0] <= 'd8;
+            dirVec[1] <= 'd4;
+            dirVec[2] <= 'd2;
+            dirVec[3] <= 'd1;
+        end
+    `endif
+
+    `ifdef INWD5
+        always_comb begin : proc_dirVec_5
+            dirVec[0] <= 'd16;
+            dirVec[1] <= 'd8;
+            dirVec[2] <= 'd4;
+            dirVec[3] <= 'd2;
+            dirVec[4] <= 'd1;
+        end
+    `endif
+
     `ifdef INWD6
         always_comb begin : proc_dirVec_6
             dirVec[0] <= 'd32;
@@ -37,34 +64,60 @@ module SobolRNGDim1 (
             dirVec[4] <= 'd2;
             dirVec[5] <= 'd1;
         end
-    `else
-        `ifdef INWD8
-            always_comb begin : proc_dirVec_8
-                dirVec[0] <= 'd128;
-                dirVec[1] <= 'd64;
-                dirVec[2] <= 'd32;
-                dirVec[3] <= 'd16;
-                dirVec[4] <= 'd8;
-                dirVec[5] <= 'd4;
-                dirVec[6] <= 'd2;
-                dirVec[7] <= 'd1;
-            end
-        `else 
-            `ifdef INWD10
-                always_comb begin : proc_dirVec_10
-                    dirVec[0] <= 'd512;
-                    dirVec[1] <= 'd256;
-                    dirVec[2] <= 'd128;
-                    dirVec[3] <= 'd64;
-                    dirVec[4] <= 'd32;
-                    dirVec[5] <= 'd16;
-                    dirVec[6] <= 'd8;
-                    dirVec[7] <= 'd4;
-                    dirVec[8] <= 'd2;
-                    dirVec[9] <= 'd1;
-                end
-            `endif
-        `endif
+    `endif
+
+    `ifdef INWD7
+        always_comb begin : proc_dirVec_7
+            dirVec[0] <= 'd64;
+            dirVec[1] <= 'd32;
+            dirVec[2] <= 'd16;
+            dirVec[3] <= 'd8;
+            dirVec[4] <= 'd4;
+            dirVec[5] <= 'd2;
+            dirVec[6] <= 'd1;
+        end
+    `endif
+
+    `ifdef INWD8
+        always_comb begin : proc_dirVec_8
+            dirVec[0] <= 'd128;
+            dirVec[1] <= 'd64;
+            dirVec[2] <= 'd32;
+            dirVec[3] <= 'd16;
+            dirVec[4] <= 'd8;
+            dirVec[5] <= 'd4;
+            dirVec[6] <= 'd2;
+            dirVec[7] <= 'd1;
+        end
+    `endif
+
+    `ifdef INWD9
+        always_comb begin : proc_dirVec_9
+            dirVec[0] <= 'd256;
+            dirVec[1] <= 'd128;
+            dirVec[2] <= 'd64;
+            dirVec[3] <= 'd32;
+            dirVec[4] <= 'd16;
+            dirVec[5] <= 'd8;
+            dirVec[6] <= 'd4;
+            dirVec[7] <= 'd2;
+            dirVec[8] <= 'd1;
+        end
+    `endif
+
+    `ifdef INWD10
+        always_comb begin : proc_dirVec_10
+            dirVec[0] <= 'd512;
+            dirVec[1] <= 'd256;
+            dirVec[2] <= 'd128;
+            dirVec[3] <= 'd64;
+            dirVec[4] <= 'd32;
+            dirVec[5] <= 'd16;
+            dirVec[6] <= 'd8;
+            dirVec[7] <= 'd4;
+            dirVec[8] <= 'd2;
+            dirVec[9] <= 'd1;
+        end
     `endif
 
     SobolRNG u_SobolRNG(
