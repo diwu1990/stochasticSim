8933c7840ae44892021c25a3dcef77b20bcf0c3f
wudidaizi <wudidaizi@vpn-pa-full-ptr-35.engr.wisc.edu>
2020-02-13 10:20:52 -0600

    update dADD_rot_lfsr

--- hardware/scu/dADD_rot_lfsr/dADD_rot_lfsr.sv
+++ hardware/scu/dADD_rot_lfsr/dADD_rot_lfsr.sv
@@ -13,7 +13,7 @@ module dADD_rot_lfsr (
     logic [`LOGINUM-1:0] sel;
     logic [`LOGINUM-1:0] cnt;
 
-    lfsr #(.NUM_BITS(`LOGINUM)) LFSR_inst_A
+    lfsr #(.NUM_BITS(3)) LFSR_inst_A
         (.clk(clk),
         .enable(1'b1),
         .i_Seed_DV(loadA),
